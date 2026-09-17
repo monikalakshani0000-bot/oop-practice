d92b49de7dc83ced28419321223de6499dafebeb
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-18 04:50:16 +0530

    Encapsulated Fields

--- Demo.java
+++ Demo.java
@@ -1,32 +1,60 @@
 class Box{
-	int length;
-	int width;
-	int height;
+	private int length;
+	private int width;
+	private int height;
+	
 	//------------methods declarations----------------
 	public void printVolume(){
 		System.out.println("Volume of the box : "+getVolume());
 	}
-	public void setSize(int length, int width, int height){
-		this.length=length;
-		this.width=width;
-		this.height=height;
-	}
-	
 	public int getVolume(){
 		return length*width*height;
 	}
+	public void setSize(int length, int width, int height){
+		setLength(length);
+		setWidth(width);
+		setHeight(height);
+	}
+	
+	public void setLength(int length){
+		this.length = length;
+	}
+	
+	public int getLength(){
+		return length;
+	}
+	
+	public void setWidth(int width){
+		this.width = width;
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+	
+	public void setHeight(int height){
+		this.height = height;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
+	
 }
-
 class Demo {
 	public static void main(String[] args) {
 		Box b1=new Box();
 		b1.setSize(12,5,3);
 		b1.printVolume(); //180
+		System.out.println();
 		
-		b1.setSize(120,50,30);
-		int volume=b1.getVolume(); //
-		System.out.println("New Volume : "+volume); //180000
+		b1.setLength(120);
+		b1.setWidth(50);
+		b1.setHeight(30);
+		
+		b1.printVolume(); //180000
+		System.out.println("New Length : "+b1.getLength());
+		System.out.println("New Width  : "+b1.getWidth());
+		System.out.println("New Height : "+b1.getHeight());
     }
 }
-
-
