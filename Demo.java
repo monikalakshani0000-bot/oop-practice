c32df5d0fd5620535f52cefe2866e514930e4132
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-17 11:04:09 +0530

    Parameterized Constructors

--- Demo.java
+++ Demo.java
@@ -1,37 +1,66 @@
-class Box{
-	//------------attribute declarations----------------
-	int length;
-	int width;
-	int height;
-	//------------methods declarations----------------
-	public void printVolume(){
-		int volume;
-		volume=length*width*height;
-		System.out.println("Volume of the box : "+volume);
+class Date{
+	int year;
+	int month;
+	int day;
+	
+	Date(){
+		year=1970;
+		month=1;
+		day=1;
+	}		
+	
+	public void printDate(){
+		System.out.println(year+"-"+month+"-"+day);
 	}
 	
-	public void setSize(int len, int wid, int hei){
-		length = len;
-		width = wid;
-		height = hei;
+	public void set(int y, int m, int d ){
+		this.year = y;
+		this.month = m;
+		this.day = d;		
 	}
+	
+	public Date(int y, int m, int d ){
+		this.year = y;
+		this.month = m;
+		this.day = d;		
+	}
+	
+	public void setYear(int y){
+		this.year = y;		
+	}
+	
+	public void setMonth(int m){
+		this.month = m;		
+	}
+	
+	public void setDay(int d){
+		this.day = d;		
+	}
+	
+	public String toString(){
+		return year+"-"+month+"-"+day;
+	}
+
+	 
 }
 class Demo {
 	public static void main(String[] args) {
-		Box b1=new Box();
-		/*b1.length=12;
-		b1.width=5;
-		b1.height=3;*/
-		b1.setSize(12,5,3);
-		b1.printVolume(); //180
+		Date d1=new Date();
+		d1.printDate(); //1970-1-1 (Default date);
 		
+		d1.set(1999,12,31);	
+		d1.printDate(); //1999-12-31
 		
+		Date d2=new Date(2020,3,14);
+		d2.printDate(); //2020-3-14
 		
-		
-		
-		
-		
-    }
+		Date d3=new Date();
+		d3.setYear(2026);
+		d3.setMonth(9);
+		d3.setDay(17);
+		System.out.println(d3.toString());//2026-9-17
+	}
 }
+
 
 
