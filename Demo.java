cf43060107eda21f6f0e39691fecf358723626cc
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-18 03:56:44 +0530

    .this

--- Demo.java
+++ Demo.java
@@ -1,28 +1,22 @@
 class Box{
-	//------------attribute declarations----------------
 	int length;
 	int width;
 	int height;
-	//------------methods declarations----------------
-	public void printVolume(){
-		int volume;
-		volume=length*width*height;
-		System.out.println("Volume of the box : "+volume);
-	}
 	
-	public void setSize(int l, int w, int h){
-		length = l;
-		width = w;
-		height = h;
+	public void print(){
+		System.out.println(this);
 	}
 }
 class Demo {
 	public static void main(String[] args) {
 		Box b1=new Box();
-		/*b1.length=12;
-		b1.width=5;
-		b1.height=3;*/
-		b1.setSize(12,5,3);
-		b1.printVolume(); //180
+		System.out.println(b1);
+		b1.print();
+		
+		System.out.println();
+		Box b2=new Box();
+		System.out.println(b2);
+		b2.print();
+		
     }
 }
