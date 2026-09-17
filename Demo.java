cb1293c7f6bf9d465ae88f445827d53b62ff9586
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-17 16:04:42 +0530

    new 1

--- Demo.java
+++ Demo.java
@@ -4,25 +4,37 @@ class Box{
 	private int height;
 	
 	public Box(){
-
 	}
 	public Box(int length, int width, int height){
 		this.length=length;
 		this.width=width;
 		this.height=height;
 	}
+	public void printVolume(){
+		int volume=length*width*height;
+		System.out.println("Volume of the box : "+volume);
+	}
 }
 class Demo {
 	public static void main(String[] args) {
 		Box[] br=new Box[3]; //Reference array, Box type
-		System.out.println(br[0]+" "+br[1]+" "+br[2]); //null null null
 		br[0]=new Box(12,5,3);
 		br[1]=new Box(120,50,30);
 		br[2]=new Box(1,2,3);
 		
-		for (int i=0 ; i<br.length ; i++){
-			System.out.println(br[i]);
+		//Insert code here to get the following output	
+		for(int i=0; i<br.length; i++){
+			Box b1=br[i];
+			b1.printVolume(); //br[i].printVolume();
 		}
-		
+		System.out.println();
+		for(Box b1 : br){
+			b1.printVolume();
+		}
 	}
 }
+/*
+Volume of the box : 180
+Volume of the box : 18000
+Volume of the box : 6
+*/
