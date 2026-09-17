63fb52e92e7d769645c3d16c84fd39074e7783fb
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-18 04:31:23 +0530

    Adding a return typed Method

--- Demo.java
+++ Demo.java
@@ -4,25 +4,29 @@ class Box{
 	int height;
 	//------------methods declarations----------------
 	public void printVolume(){
-		int volume;
-		volume=length*width*height;
-		System.out.println("Volume of the box : "+volume);
+		System.out.println("Volume of the box : "+getVolume());
 	}
 	public void setSize(int length, int width, int height){
 		this.length=length;
 		this.width=width;
 		this.height=height;
 	}
+	
+	public int getVolume(){
+		return length*width*height;
+	}
 }
+
 class Demo {
 	public static void main(String[] args) {
 		Box b1=new Box();
 		b1.setSize(12,5,3);
-		System.out.println("Length : "+b1.length);
-		System.out.println("Width  : "+b1.width);
-		System.out.println("Height : "+b1.height);
-
-		b1.printVolume(); //
+		b1.printVolume(); //180
+		
+		b1.setSize(120,50,30);
+		int volume=b1.getVolume(); //
+		System.out.println("New Volume : "+volume); //180000
     }
 }
+
 
