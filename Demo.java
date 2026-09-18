55f897a795bfc907d01ffee822a02cc3729591fd
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-18 12:55:01 +0530

    constructorr 02

--- Demo.java
+++ Demo.java
@@ -1,18 +1,41 @@
-class A{
-	int a;
-}
-class B{
-	int b;
-	B(int i){
-		b=i;
+class Box{
+	int length;
+	int width;
+	int height;
+	
+	public Box(int length, int width, int height){
+		this.length=length;
+		this.width=width;
+		this.height=height;
+		System.out.println("Box(int,int,int)");
+	}
+	public Box(int length){
+		this.length=length;
+		this.width=length;
+		this.height=length;
+		System.out.println("Box(int)");
+	}
+
+
+	public Box(){
+		this.length=1;
+		this.width=1;
+		this.height=1;
+		System.out.println("Box()");
+	}
+	public void printVolume(){
+		System.out.println("Volume of the box : "+(length*width*height));
 	}
 }
 class Demo {
 	public static void main(String[] args) {
-		A a1=new A();
-		A a2=new A(100);
+		Box b1=new Box(); //calling default constructor
+		b1.printVolume();
 		
-		B b1=new B();
-		B b2=new B(100);
+		Box b2=new Box(10);
+		b2.printVolume();
+		
+		Box b3=new Box(12,5,3);
+		b3.printVolume();
     }
 }
