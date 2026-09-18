b0ea55ab836d953bb80508625fa327e62e0dd531
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-18 11:13:54 +0530

    constructor

--- Demo.java
+++ Demo.java
@@ -1,68 +1,22 @@
-class Student{
-	private String id;
-	private String name;
-	private int prf;
-	private int dbms;
+class Box{
+	int length;
+	int width;
+	int height;
 	
-	public void printStudentDetails(){
-		System.out.println(id+"\t"+name+"\t"+prf+"\t"+dbms);
-	}
+	public Box(){
+		length=12;
+		width=5;
+		height=3;
+		System.out.println("Constructor... Box()");
+	}	
 	
-	public void setStudentDetails(String id, String name){
-		this.id = id;
-		this.name = name;
-	}
-	
-	public void setMarks(int prf, int dbms){
-		this.prf = prf;
-		this.dbms = dbms;
-	}
-		
-	public void setPrfMarks(int prfM){
-		this.prf = prfM;
-	}
-	
-	public void setDbmsMarks(int dbmsM){
-		this.dbms = dbmsM;
-	}
-	
-	public String getId(){
-		return id;
-	}
-	
-	public String getName(){
-		return name;
-	}
-	
-	public int getPrfMarks(){
-		return prf;
-	}
-	
-	public int getDbmsMarks(){
-		return dbms;
-	}
-
-	
-	public int getTotal(){
-		return prf+dbms;
-	}
 }
-
 class Demo {
 	public static void main(String[] args) {
-		Student s1=new Student();
-		s1.setStudentDetails("S0001","Nimal");
-		s1.setMarks(56,70);
-		s1.printStudentDetails(); //S0001	Nimal	56 	70
-		
-		s1.setPrfMarks(89);
-		s1.setDbmsMarks(90);
-		s1.printStudentDetails(); //S0001	Nimal	89	90
-		
-		System.out.println("Student Id   : "+s1.getId());	//S0001
-		System.out.println("Student Name : "+s1.getName());	//Nimal
-		System.out.println("PRF Marks    : "+s1.getPrfMarks());//89
-		System.out.println("DBMS Marks   : "+s1.getDbmsMarks());//90
-		System.out.println("Total Marks  : "+s1.getTotal());//179
+		Box b1=new Box();
+
+		System.out.println("New Length : "+b1.length);	//12
+		System.out.println("New Width  : "+b1.width);	//5
+		System.out.println("New Height : "+b1.height);	//3
     }
 }
